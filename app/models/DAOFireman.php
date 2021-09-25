1c220afcf99b11967be531c5db85f1e3c4426a7b
Quentin CHAVET (TakeN) <q.chavet@eleve.leschartreux.net>
2021-09-25 17:21:10 +0200

    Feature & refactoring : DAOFireman and Fireman file

--- app/models/DAOFireman.php
+++ app/models/DAOFireman.php
@@ -6,7 +6,7 @@
 
     /**
      * DAOFireman which contains different kind of functions
-     * 
+     *
      * @author TakeN
      */
     class DAOFireman {
@@ -15,9 +15,9 @@
 
         /**
          * Constructor of the class DAOPompier
-         * 
+         *
          * @param type $connexion
-         * 
+         *
          */
         public function __construct($connexion) {
 
@@ -29,71 +29,295 @@
 
         /**
          * Retrieve one fireman with the matricule
-         * 
+         *
          * @param int $matricule
-         * @return Fireman 
+         *
+         * @return Fireman
          */
         public function retrieveFireman($matricule) : ?Fireman{
 
-            $requestSQL = "SELECT * FROM pompiers WHERE Matricule=:matricule";
 
-            $preparedStatement = $this->connexion->prepare($requestSQL);
+            try {
 
-            $preparedStatement->bindParam("matricule",$matricule);
 
-            $preparedStatement->execute();
+                $requestSQL = "SELECT * FROM pompiers WHERE Matricule =:matricule";
 
-            $fireman = $preparedStatement->fetchObject("Fireman");
+                $preparedStatement = $this->connexion->prepare($requestSQL);
 
-            return $fireman;
+                $preparedStatement->bindParam(':matricule', $matricule);
+
+                $preparedStatement->execute();
+
+                $data = $preparedStatement->fetchObject('app\models\\Fireman');
+
+                return $data;
+
+
+            } catch (\Exception $erreur) {
+
+                echo "<pre>".print_r($erreur,true)."</pre>";
+
+            }
+
 
         }
-    
+
+
+
+        /**
+        * Retrieve number of firemen in database
+        *
+        * @return int
+        */
+        public function countFiremen() : int {
+
+
+            try {
+
+
+                $requestSQL = "SELECT * FROM pompiers";
+
+                $preparedStatement = $this->connexion->prepare($requestSQL);
+
+                $preparedStatement->execute();
+
+                $countFiremen = 0;
+
+                while ($data = $preparedStatement->fetchObject('app\models\\Fireman')) {
+
+                    $countFiremen += 1;
+
+                }
+
+                return $countFiremen;
+
+
+            } catch (\Exception $erreur) {
+
+                echo "<pre>".print_r($erreur,true)."</pre>";
+
+            }
+        }
+
+
+
+        /**
+        * Retrieve the ten first firemen from the database
+        *
+        * @param int $limit
+        * @param int $offset
+        *
+        * @return array $firemen
+        */
+        public function findAllFiremen($limit, $offset) : Array {
+
+
+            try {
+
+
+                $requestSQL = "SELECT * FROM pompiers LIMIT ? OFFSET ?";
+
+                $preparedStatement = $this->connexion->prepare($requestSQL);
+
+                $preparedStatement->bindValue(1, $limit, \PDO::PARAM_INT);
+                $preparedStatement->bindValue(2, $offset, \PDO::PARAM_INT);
+                $preparedStatement->execute();
+
+                $firemen = array();
+
+                while ($data = $preparedStatement->fetchObject('app\models\\Fireman')) {
+
+                    array_push($firemen, $data);
+
+                }
+
+                return $firemen;
+
+
+            } catch (\Exception $erreur) {
+
+                echo "<pre>".print_r($erreur,true)."</pre>";
+
+            }
+
+
+        }
+
+
+
+        /**
+         *Saving one fireman in the database
+         *
+         * @param Fireman $fireman
+         * 
+         * @return int $linesAdded
+         */
+        public function saveFireman(Fireman $fireman) : int {
+
+            try {
+
+
+                $requestSQL = "INSERT INTO pompiers VALUES (?,?,?,?,?,?,?,?)";
+
+                $preparedStatement = $this->connexion->prepare($requestSQL);
+
+                $preparedStatement->bindValue(1,$fireman->getMatricule());
+                $preparedStatement->bindValue(2,$fireman->getPrenom());
+                $preparedStatement->bindValue(3,$fireman->getNom());
+                $preparedStatement->bindValue(4,$fireman->getChefAgret());
+                $preparedStatement->bindValue(5,$fireman->getDateNaissance());
+                $preparedStatement->bindValue(6,$fireman->getNumCaserne());
+                $preparedStatement->bindValue(7,$fireman->getCodeGrade());
+                $preparedStatement->bindValue(8,$fireman->getMatriculeResponsable());
+
+                $preparedStatement->execute();
+
+                $linesAdded = $preparedStatement->rowCount();
+
+                return $linesAdded;
+
+
+            } catch (\Exception $erreur) {
+
+                echo "<pre>".print_r($erreur,true)."</pre>";
+
+            }
+
+
+        }
+
+
+
+        /**
+         * Delete  the given fireman from the database
+         * 
+         * @param Fireman $fireman
+         * 
+         * @return int $linesDeleted
+         */
+        public function removeFireman(Fireman $fireman) : int {
+
+
+            try {
+
+                $matriculeToDelete = $fireman->getMatricule();
+
+                $requestSQL = "DELETE FROM pompiers WHERE Matricule = :matriculeToDelete";
+
+                $preparedStatement = $this->connexion->prepare($requestSQL);
+
+                $preparedStatement->bindParam(':matriculeToDelete', $matriculeToDelete);
+
+                $preparedStatement->execute();
+
+                $linesDeleted = $preparedStatement->rowCount();
+
+                return $linesDeleted;
+
+
+            } catch (\Exception $erreur) {
+
+                echo "<pre>".print_r($erreur,true)."</pre>";
+
+            }
+
+
+        }
+
+
+
+        /**
+         * Updating information about one fireman
+         * 
+         * @param Fireman $fireman
+         * 
+         * @return int $linesModified
+         */
+        public function updateFireman(Fireman $fireman) : int {
+
+
+            try {
+
+
+                $requestSQL = "UPDATE pompiers SET Prenom = :prenom, Nom = :nom, ChefAgret = :chefAgret, DateNaissance = :dateNaissance, NumCaserne = :numCaserne, CodeGrade = :codeGrade, MatriculeResponsable = :matriculeResponsable WHERE Matricule = :matricule";
+
+                $preparedStatement = $this->connexion->prepare($requestSQL);
+
+                $preparedStatement->bindValue(':prenom', $fireman->getPrenom());
+                $preparedStatement->bindValue(':nom', $fireman->getNom());
+                $preparedStatement->bindValue(':chefAgret', $fireman->getChefAgret());
+                $preparedStatement->bindValue(':dateNaissance', $fireman->getDateNaissance());
+                $preparedStatement->bindValue(':numCaserne', $fireman->getNumCaserne());
+                $preparedStatement->bindValue(':codeGrade', $fireman->getCodeGrade());
+                $preparedStatement->bindValue(':matriculeResponsable', $fireman->getMatriculeResponsable());
+                $preparedStatement->bindValue(':matricule', $fireman->getMatricule());
+
+                $preparedStatement->execute();
+
+                $linesModified = $preparedStatement->rowCount();
+
+                return $linesModified;
+
+
+            } catch (\Exception $erreur) {
+
+                echo "<pre>".print_r($erreur,true)."</pre>";
+
+            }
+
+
+        }
+
+
+
+        /**
+         * 
+         * 
+         * 
+         */
+        public function findBarracksFromFireman(Fireman $fireman): ?Caserne{
+
+
+            try {
+
+
+                $matriculeFireman = $fireman->getMatricule();
+
+                $requestSQL = "SELECT NumCaserne FROM pompiers WHERE Matricule = :matricule";
+
+                $preparedStatement = $this->connexion->prepare($requestSQL);
+
+                $preparedStatement->bindParam(':matricule', $matriculeFireman);
+
+                $preparedStatement->execute();
+
+                $data = $preparedStatement->fetch();
+
+                
+                $requestSQL = "SELECT * FROM casernes WHERE NumCaserne = :numCaserne";
+
+                $preparedStatement = $this->connexion->prepare($requestSQL);
+
+                $preparedStatement->bindParam(':numCaserne', $data[0]);
+
+                $preparedStatement->execute();
+
+                $data = $preparedStatement->fetchObject('app\models\\Caserne');
+
+                return $data;
+
+
+            } catch (\Exception $erreur) {
+
+                echo "<pre>".print_r($erreur,true)."</pre>";
+
+            }
+
+
+        }
+
+
     }
 
-
-
-    //     public function save(Pompier $pompier) : void {
-
-    //     }
-
-
-
-
-    //     public function update(Pompier $pompier) : void {
-
-    //     }
-
-
-
-
-    //     public function remove(Pompier $pompier) : void {
-
-    //     }
-
-
-
-
-    //     public function findAll($offset = 0, $limit = 10) : Array {
-
-    //     }
-
-
-    //     /**
-    //      * Retrieve number of firemen in database
-    //      * @return int
-    //      */
-    //     public function count() : int {
-
-    //     }
-
-
-
-
-    //     public function findFireHouseFromFireman(Pompier $pompier): ?Pompier{
-            
-    //     }
-
-    // }
 
 ?>
--- app/models/Fireman.php
+++ app/models/Fireman.php
@@ -6,47 +6,126 @@
 
     /**
      * Class Fireman
-     * 
+     *
      * @author TakeN
      */
     class Fireman {
 
-        private $matricule;
-        private $prenom;
-        private $nom;
-        private $chefAgret;
-        private $dateNaissance;
-        private $numCaserne;
-        private $codeGrade;
-        private $matriculeRespo;
+        private $Matricule;
+        private $Prenom;
+        private $Nom;
+        private $ChefAgret;
+        private $DateNaissance;
+        private $NumCaserne;
+        private $CodeGrade;
+        private $MatriculeResponsable;
 
 
-        /**
-         * Constructor of the class Fireman
-         * 
-         * @param string $matricule
-         * @param string $prenom
-         * @param string $nom
-         * @param char $chefAgret
-         * @param datetime $dateNaissance
-         * @param int $numCaserne
-         * @param string $codeGrade
-         * @param string $matriculeRespo
-         * 
-         */
-        public function __construct($matricule,$prenom,$nom,$chefAgret,$dateNaissance,$numCaserne,$codeGrade,$matriculeRespo) {
-            
 
-            $this->matricule = $matricule;
-            $this->prenom = $prenom;
-            $this->nom = $nom;
-            $this->chefAgret = $chefAgret;
-            $this->dateNaissance = $dateNaissance;
-            $this->numCaserne = $numCaserne;
-            $this->codeGrade = $codeGrade;
-            $this->matriculeRespo = $matriculeRespo;
+        public function __construct() {}
+ 
+        
+
+        // ==================== Get functions for private properties ====================
+        public function getMatricule() {
+
+            return $this->Matricule;
+        }
+
+        public function getPrenom() {
+
+            return $this->Prenom;
+        }
+
+        public function getNom() {
+
+            return $this->Nom;
+        }
+
+        public function getChefAgret() {
+
+            return $this->ChefAgret;
+        }
+
+        public function getDateNaissance() {
+
+            return $this->DateNaissance;
+        }
+
+        public function getNumCaserne() {
+
+            return $this->NumCaserne;
+        }
+
+        public function getCodeGrade() {
+
+            return $this->CodeGrade;
+        }
+
+        public function getMatriculeResponsable() {
+
+            return $this->MatriculeResponsable;
+        }
+
+
+
+        // ==================== Set functions for private properties ====================
+        public function setMatricule($matricule) {
+
+            $this->Matricule = $matricule;
+            return $this;
 
         }
+
+        public function setPrenom($prenom) {
+
+            $this->Prenom = $prenom;
+            return $this;
+
+        }
+
+        public function setNom($nom) {
+
+            $this->Nom = $nom;
+            return $this;
+
+        }
+
+        public function setChefAgret($chefAGret) {
+
+            $this->ChefAgret = $chefAGret;
+            return $this;
+
+        }
+
+        public function setDateNaissance($dateNaissance) {
+
+            $this->DateNaissance = $dateNaissance;
+            return $this;
+
+        }
+
+        public function setNumCaserne($numCaserne) {
+
+            $this->NumCaserne = $numCaserne;
+            return $this;
+
+        }
+
+        public function setCodeGrade($codeGrade) {
+
+            $this->CodeGrade = $codeGrade;
+            return $this;
+
+        }
+
+        public function setMatriculeResponsable($matriculeResponsable) {
+
+            $this->MatriculeResponsable = $matriculeResponsable;
+            return $this;
+
+        }
+
 
     }
 
