c929442aacb543aa7e168ba7eff9eada11f98554
h.belhadjmansour <h.belhadjmansour@eleve.leschartreux.net>
2021-09-28 15:57:44 +0200

    2 fonctions

--- app/models/Caserne.php
+++ app/models/Caserne.php
@@ -8,6 +8,28 @@
 
 namespace app\models;
 
-class Caserne {
+class Barrack {
+    private $numCaserne;
+    private $adresse;
+    private $cp;
+    private $ville;
+    private $codeTypeC;
+
+    /**
+     * Construct of FireHouse
+     * 
+     * @param int $numCaserne
+     * @param string $adresse
+     * @param string $cp
+     * @param string $ville
+     * @param string $codeTypeC
+    */
+    public function __contrusct($numCaserne, $adresse, $cp, $ville, $codeTypeC){
+        $this->numCaserne = $numCaserne;
+        $this->adresse = $adresse;
+        $this->cp = $cp;
+        $this->ville = $ville;
+        $this->codeTypeC = $codeTypeC;
+    }
 
 }
--- app/models/DAOCaserne.php
+++ app/models/DAOCaserne.php
@@ -1,56 +1,145 @@
 <?php
 
 /**
-* Description of caserne
-*
-* @author Haythem
-*/
+ * Description of barrack
+ *
+ * @author Haythem
+ */
 
 namespace app\models;
 
-class DAOCaserne {
-
-    private $cnx;
-
-    public function __contrusct($connect){
-
+class DAOBarrack
+{
+    /**
+     * Constructor of DAOBarrack
+     */
+    public function __contrusct($connexion)
+    {
+        $this->connexion = $connexion;
     }
 
+    /**
+     * Retrieve a barrack by id
+     * 
+     * @param int $numBarrack
+     * @return Barrack
+    */
     //? veut dire qu'il ne peut rien retourner (NULL)
-    public function find($id) : ?Caserne {
+    public function find($numBarrack): ?Barrack
+    {
 
-    }
-
-    public function save(Caserne $caserne) : void {
-
-    }
-
-    public function update(Caserne $caserne) : void {
+        try{
+        $SQL = 'SELECT * FROM barrack WHERE NumBarrack = :numBarrack;';
         
+        $prepareStatement = $this->connection->prepare($SQL);
+        $prepareStatement->bindParam("numBarrack", $numBarrack);
+        $prepareStatement->execute();
+
+        $barrack = $prepareStatement->fetchObject("Barrack", $ctor_args);
+        return $barrack;
+
+        } catch (Exception $e){ // Deux exceptions à mettre dans le controller
+            echo "<pre>".print_r($e, true)."</pre>";
+        }
     }
 
-    public function remove(Caserne $caserne) : void {
+    public function save(Barrack $barrack): void
+    {
+        try {
+            
+        $SQL = 'INSERT INTO  barrack VALUES ()'; 
         
+        $prepareStatement = $this->connection->prepare($SQL);
+        
+        } catch (Exception $e){ // Deux exceptions à mettre dans le controller
+            echo "<pre>".print_r($e, true)."</pre>";
+        }
+    }
+
+    /**
+     * Update a data into barrack
+    */
+    public function update(Barrack $barrack): void
+    {
+        try {
+            
+        $SQL = 'UPDATE barrack SET '; 
+        
+        $prepareStatement = $this->connection->prepare($SQL);
+        
+
+        } catch (Exception $e){ // Deux exceptions à mettre dans le controller
+            echo "<pre>".print_r($e, true)."</pre>";
+        }
+    }
+
+    public function remove(Barrack $barrack): void
+    {
+        try {
+            
+            $SQL = 'DELETE FROM barrack WHERE'; 
+            
+            $prepareStatement = $this->connection->prepare($SQL);
+            
+            } catch (Exception $e){ // Deux exceptions à mettre dans le controller
+                echo "<pre>".print_r($e, true)."</pre>";
+            }
     }
 
     /**
      * @param int $offset
      * @param int $limit
-     * @return array<Caserne>
-    */
-    public function findALl($offset=0, $limit=10) : Array {
+     * @return array<Barrack>
+     */
+    public function findAll($offset = 0, $limit = 10): array
+    {
+        try{
+            $SQL = 'SELECT * FROM barrack LIMIT ' .$limit. ';';
 
+            $prepareStatement = $this->connection->prepare($SQL);
+            $barracks = array();
+
+            while($data = $prepareStatement->fetchObject("Barrack")){
+                array_push($barracks, $data);
+            }
+            return $barracks;
+
+            //autre méthode:
+            // $barracks = [];
+            // while($data = $prepareStatement->fetch(PDO::FETCH_ASSOC)){
+            //     $barracks[] = $data;
+            // }
+            // return $barracks;
+        } catch (Exception $e){ // Deux exceptions à mettre dans le controller
+            echo "<pre>".print_r($e, true)."</pre>";
+        }
     }
 
     /**
-     * Retrieve number of FireHouse in DB
+     * Retrieve number of Barrack in DB
      * @return int
-    */
-    public function count() : int {
+     */
+    public function count(): int
+    {
+        try {
+            
+            $SQL = 'SELECT * FROM barracks;'; 
+            
+            $prepareStatement = $this->connection->prepare($SQL);
 
+            $countBarrack = 0;
+
+            while($data = $prepareStatement->fetch()){
+                $countBarrack = $countBarrack + 1;
+            }
+            return $countBarrack;
+
+            } catch (Exception $e){ // Deux exceptions à mettre dans le controller
+                echo "<pre>".print_r($e, true)."</pre>";
+            }
     }
 
-    public function findFireMenFromFireHouse(Caserne $caserne) : ?Caserne {
-
+    public function findFireMenFromBarrack(Barrack $barrack): ?Barrack
+    {
     }
 }
